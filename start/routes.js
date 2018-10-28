79cf257abffd9e47e690b5a781cfb3d50dd2ab19
chenjing <1002931123@qq.com>
2018-10-28 19:20:51 +0800

    add request-response 基本data接收的方式

--- start/routes.js
+++ start/routes.js
@@ -14,17 +14,29 @@
 */
 
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
-const Database=use('Database')
+
 const Route = use('Route')
 
 Route.on('/').render('welcome')
 
-// Route.get('/hello',({request})=>{
-//   // return `hello ${request.input('name')}`
-//
-// })
+/*先写一个路由. 在写一个控制器  context 里面有很多东西. 但是我们只要使用request ES5的语法解构出来*/
 
-Route.get('/hello','HelloController.render')
-Route.get('/posts',async ()=>{
-  return await Database.table('posts').select('*')
-})
+// Route.get('/posts',(context)=>{})
+Route.get('/gets', ({request}) => request.get())
+
+// 获取post 请求参数
+Route.post('/posts', ({request}) => request.post())
+
+//  all 可以获取get 和post 两种方式的 请求参数
+Route.post('/alls', ({request}) => request.all())
+
+// only 指定获取的参数
+Route.post('/only', ({request}) => request.only(["xxx","ccc"]))
+
+// excpet 指定除去xxx参数 以外的参数
+Route.post('/excpet', ({request}) => request.excpet(["xxxx"]))
+
+//如果参数里面没有这个值. 给他一个默认值
+Route.post('/input', ({request}) => request.input("name","默认值23"))
+
+//
