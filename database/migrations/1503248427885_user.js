726635f8c72f4b24a2e5368194ff07802c0b2d39
chenjing <1002931123@qq.com>
2018-10-30 00:35:40 +0800

    add db

--- database/migrations/1503248427885_user.js
+++ database/migrations/1503248427885_user.js
@@ -4,16 +4,20 @@
 const Schema = use('Schema')
 
 class UserSchema extends Schema {
+  //想要执行的事情
   up () {
     this.create('users', (table) => {
+      // id 值
       table.increments()
+      // notNullable() 不可为空 .unique() 唯一值
       table.string('username', 80).notNullable().unique()
       table.string('email', 254).notNullable().unique()
       table.string('password', 60).notNullable()
+      // 会记录数据跟新的那个日期. 和数据最近更新的那个日期
       table.timestamps()
     })
   }
-
+  // 回滚的事务
   down () {
     this.drop('users')
   }
--- database/migrations/1540830516140_post_schema.js
+++ database/migrations/1540830516140_post_schema.js
@@ -1,19 +1,17 @@
 'use strict'
 
 /** @type {import('@adonisjs/lucid/src/Schema')} */
+// 创建数据表的时候 是knex 的结构来创建的
 const Schema = use('Schema')
 
 class PostSchema extends Schema {
-  // up 是创建数据表
   up () {
     this.create('posts', (table) => {
       table.increments()
-      table.string('title')
-      table.text('content','longtext')
       table.timestamps()
     })
   }
-  //删除这个表
+
   down () {
     this.drop('posts')
   }
